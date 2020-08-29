10b0c556ec75067888cc0c50c06177a87fc4fc8f
Pavel <pavelputin2003@yandex.ru>
2020-08-29 14:26:22 +0300

    added man's photo to header_inner and made header_content media transition from bottom to left side of header

---
+++ src/blocks/header/__inner/header__inner.js
@@ -0,0 +1,13 @@
+const headerInner = document.querySelector(`.header__inner`);
+const headerContent = document.querySelector(`.header__content`);
+
+setTimeout(setHeaderInnerBeforeBottom, 50)
+window.addEventListener(`resize`, setHeaderInnerBeforeBottom, false);
+
+
+function setHeaderInnerBeforeBottom() {
+  if (document.documentElement.clientWidth < 1024) {
+    headerInner.style.setProperty(`--bottom-for-before`, `${headerContent.clientHeight}px`);
+  } 
+}
+
